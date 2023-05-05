2ebc2ada171a1bf2a95e34fda1bc33b5a1344075
Piotr Pelc <piotrpelc83@gmail.com>
2023-05-05 15:15:36 +0200

    Intro

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String greeting = "Hello World!";
+        String name = "Piotr";
+        int age = 40;
+
+        System.out.println(greeting);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(name.charAt(0));
+    }
+}
