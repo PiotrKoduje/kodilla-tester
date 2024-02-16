356566a8aae3fa2ab3e974497118a4b403e95126
Piotr Pelc <piotrpelc83@gmail.com>
2024-02-16 19:31:03 +0100

    Exceptions - homework

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/exception/homework/Warehouse.java
@@ -0,0 +1,21 @@
+package com.kodilla.stream.exception.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Warehouse {
+    private List<Order> orders = new ArrayList<>();
+
+    public void addOrder(Order order){
+        orders.add(order);
+    }
+
+    public Order getOrder(String number) throws OrderDoesntExistException {
+        Optional<Order> result = orders
+                .stream()
+                .filter(o -> o.getNumber().equals(number))
+                .findFirst();
+        return result.orElseThrow(OrderDoesntExistException::new);
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/exception/homework/WarehouseApp.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.exception.homework;
+
+import java.util.function.DoubleToIntFunction;
+
+public class WarehouseApp{
+    public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("121"));
+        warehouse.addOrder(new Order("122"));
+        warehouse.addOrder(new Order("123"));
+        warehouse.addOrder(new Order("124"));
+        warehouse.addOrder(new Order("125"));
+
+        try {
+            System.out.println("Wybrano zamówienie nr: " + warehouse.getOrder("422").getNumber());
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Takie zamówienie nie istnieje");
+        }
+    }
+}
